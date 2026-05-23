f9e853672d59faef75d522d5de3fa8539020fa57
Szymon Nowakowski <szymon.nowakowski2807@gmail.com>
2026-05-23 15:19:57 +0200

    Program, that presentates ability to use static classes and methods

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Narzedzia_matematyczne_klasa_statyczna;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj km do przeliczenia na mile morkski: ");
+        double km = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Podaj temperature C do przeliczenia na F: ");
+        double tempC = Convert.ToDouble(Console.ReadLine());
+
+        double mileMorskie = Konwerter.KilometryNaMile(km);
+        Console.WriteLine($"{km}km to {mileMorskie} mil morskich.");
+        
+        double tempF = Konwerter.CelsjuszToFahrenheit(tempC);
+        Console.WriteLine($"{tempC}*C to {tempF} fahrenheit.");
+    }
+}
